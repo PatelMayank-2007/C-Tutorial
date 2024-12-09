df987c1b06fe0a1c3285484e305227c12dede074
mayank <patelmayank232007@gmail.com>
2024-12-09 16:47:43 +0530

    second commit

---
+++ mydirectory/tutorial_17_goto_stat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+ int main(int argc, char const *argv[])
+ {
+   // label:
+   // printf("we are inside label\n");
+   // printf("hello world\n");
+   // goto label;
+   
+  int i,num;
+   for ( i = 0; i < 8; i++)
+      {
+         printf("%d",i);
+         for (int j = 0; j < 8; j++)      
+         {
+            printf("enter the number.Enter 0 to exit program:");
+            scanf("%d",&num);
+            if (num == 0){
+               goto end;
+            }            
+         }
+               
+   }
+   end:
+   
+   return 0;
+   
+  }
+ 
